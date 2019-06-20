1c0cce5558fd76bffb559e9c73d25cc68951e306
rbslandau <credential.helper>
2019-06-20 22:57:24 +0200

    slight changes

--- Prep/0_install_packgs.R
+++ Prep/0_install_packgs.R
@@ -1,3 +1,3 @@
-pkgs <- readLines(file("https://raw.githubusercontent.com/andreasLD/workshop-sefs11/master/Prep/packages_to_install.txt", "r"))
+pkgs <- readLines(file("https://raw.githubusercontent.com/andreasLD/workshop-sefs11/master/Prep/packages_to_install.txt?token=AGEV6IWVS6XCZEIR2WPIYMC5CUWWW", "r"))
 str(pkgs)
 install.packages(pkgs)
