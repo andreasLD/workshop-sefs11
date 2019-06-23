8bd9cd43a876fb5710653ee92a48279c07468d3b
MorLink <link@uni-landau.de>
2019-06-23 14:55:47 +0200

    ## Moritz added packages in .txt and the link to install 'piecewiseSEM' from github

--- Prep/0_install_packgs.R
+++ Prep/0_install_packgs.R
@@ -1,3 +1,6 @@
 pkgs <- readLines(file("https://raw.githubusercontent.com/andreasLD/workshop-sefs11/master/Prep/packages_to_install.txt?token=AGEV6IWVS6XCZEIR2WPIYMC5CUWWW", "r"))
 str(pkgs)
 install.packages(pkgs)
+
+## piecewiseSEM from GitHub, needs R-Version 3.5 and newer
+install_github("jslefche/piecewiseSEM@devel", build_vignette = TRUE)
